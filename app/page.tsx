99a232d6401cead4105e9f2497dc4b4a7f22e68a
Tobias Leinß <tobias@leinss.xyz>
2024-10-29 15:35:29 +0100

    Add leaderboard content styling

--- app/page.tsx
+++ app/page.tsx
@@ -7,66 +7,118 @@ import { Leaderboard } from "@/components/dashboard/Leaderboard";
 import { CohortProjects } from "@/components/dashboard/CohortProjects";
 
 export interface Token {
-  name: string;
-  symbol: string;
-  price: number;
-  priceChange: number;
-  volume: string;
-  marketCap: string,
-  totalSupply: string,
-  contractAddress: string,
-  description: string,
+    name: string;
+    symbol: string;
+    price: number;
+    priceChange: number;
+    volume: string;
+    marketCap: string;
+    totalSupply: string;
+    contractAddress: string;
+    description: string;
+}
+
+export interface TokenHolder {
+    address: string;
+    holdings: number;
+    stakedAmount: number;
+    votingParticipation: string;
 }
 
 export default function Home() {
-  const tokens: Token[] = [
-    {
-      name: "RGCVII",
-      symbol: "RGCVII",
-      price: 1234.56,
-      priceChange: 5.67,
-      volume: "45.6M",
-      marketCap: "789.1M",
-      totalSupply: "1,000,000,000",
-      contractAddress: "0x68f2...abc",
-      description: "In a land ruled by tiny paws, the hamsters empire. With bravery and wit, they sail the Uniswap seas, conquer the Dune Desert, and protect their kingdom through DAOhaus. Join them on their quest to defeat Moloch! In a land ruled by tiny paws, the hamsters empire. With bravery and wit, they sail the Uniswap seas, conquer the Dune Desert, and protect their kingdom through DAOhaus. Join them on their quest to defeat Moloch!",
-    },
-  ];
+    const selectedTokenSymbol = "RGCVII";
 
-  return (
-    <div className="flex flex-col min-h-screen text-white">
-      <main className="flex-1 bg-gradient-to-r from-[#1A202C] to-[#40557b]">
-        <div className="container mx-auto p-4 sm:p-6 flex flex-col gap-4 h-full">
-          <h1 className="text-5xl text-rg-red font-serif my-8">RGC Token Dashboard</h1>
-          <UserWallet tokens={tokens} />
-          <div className="grid grid-cols-3 gap-4 auto-rows-[400px]">
-            <TokenSupply tokens={tokens} />
-            <TokenLore tokens={tokens} />
-            <TokenManagement />
-            <Leaderboard />
-          </div>
-          <CohortProjects />
+    const tokens: Token[] = [
+        {
+            name: "RGCVII",
+            symbol: "RGCVII",
+            price: 1234.56,
+            priceChange: 5.67,
+            volume: "45.6M",
+            marketCap: "789.1M",
+            totalSupply: "1,000,000,000",
+            contractAddress: "0x68f2...abc",
+            description:
+                "In a land ruled by tiny paws, the hamsters empire. With bravery and wit, they sail the Uniswap seas, conquer the Dune Desert, and protect their kingdom through DAOhaus. Join them on their quest to defeat Moloch! In a land ruled by tiny paws, the hamsters empire. With bravery and wit, they sail the Uniswap seas, conquer the Dune Desert, and protect their kingdom through DAOhaus. Join them on their quest to defeat Moloch!",
+        },
+    ];
+    const tokenHolders: TokenHolder[] = [
+        {
+            address: "0x68f2...abc",
+            holdings: 55_555,
+            stakedAmount: 0,
+            votingParticipation: "5/12",
+        },
+        {
+            address: "0x78f2...abd",
+            holdings: 123_555,
+            stakedAmount: 120_000,
+            votingParticipation: "12/12",
+        },
+        {
+            address: "0x88f2...abe",
+            holdings: 987_123_555,
+            stakedAmount: 150_000,
+            votingParticipation: "0/12",
+        },
+        {
+            address: "0x98f2...abf",
+            holdings: 10,
+            stakedAmount: 0,
+            votingParticipation: "0/12",
+        },
+        {
+            address: "0x1232...aca",
+            holdings: 1337,
+            stakedAmount: 337,
+            votingParticipation: "3/12",
+        },
+        {
+            address: "0x2132...fcc",
+            holdings: 1_000_000_217,
+            stakedAmount: 1_000_000_217,
+            votingParticipation: "11/12",
+        },
+    ];
 
+    return (
+        <div className="flex flex-col min-h-screen text-white">
+            <main className="flex-1 bg-gradient-to-r from-[#1A202C] to-[#40557b]">
+                <div className="container mx-auto p-4 sm:p-6 flex flex-col gap-4 h-full">
+                    <h1 className="text-5xl text-rg-red font-serif my-8">
+                        RGC Token Dashboard
+                    </h1>
+                    <UserWallet tokens={tokens} />
+                    <div className="grid grid-cols-3 gap-4 auto-rows-[400px]">
+                        <TokenSupply tokens={tokens} />
+                        <TokenLore tokens={tokens} />
+                        <TokenManagement />
+                        <Leaderboard
+                            selectedToken={selectedTokenSymbol}
+                            tokenHolders={tokenHolders}
+                        />
+                    </div>
+                    <CohortProjects />
+                </div>
+            </main>
+
+            <footer className="py-4 flex justify-center bg-[#1A202C]">
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="https://app.charmverse.io/raidguild-cohort-season-7/hamsterized-token-dashboard-0771957381100139"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="https://nextjs.org/icons/globe.svg"
+                        alt="Globe icon"
+                        width={16}
+                        height={16}
+                    />
+                    Project Details →
+                </a>
+            </footer>
         </div>
-      </main>
-
-      <footer className="py-4 flex justify-center bg-[#1A202C]">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://app.charmverse.io/raidguild-cohort-season-7/hamsterized-token-dashboard-0771957381100139"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Project Details →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
--- components/dashboard/Leaderboard.tsx
+++ components/dashboard/Leaderboard.tsx
@@ -1,19 +1,75 @@
-import React from "react";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { TokenHolder } from "@/app/page";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-const Leaderboard = () => {
-  return (
-    <Card className="bg-white/10 backdrop-blur-sm border-gray-600 col-span-2">
-      <CardHeader>
-        <CardTitle className="text-gray-200">Leaderboard</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <div className="h-[140px] flex items-center justify-center bg-white/5 rounded-lg">
-          <p className="text-gray-400">list</p>
-        </div>
-      </CardContent>
-    </Card>
-  )
-}
+const Leaderboard = ({
+    selectedToken,
+    tokenHolders,
+}: {
+    selectedToken: string;
+    tokenHolders: TokenHolder[];
+}) => {
+    return (
+        <Card className="bg-gradient-to-b from-[#262f41] to-[#475778] backdrop-blur-sm border-white col-span-2 overflow-y-auto">
+            <CardHeader className="border-b border-white">
+                <CardTitle className="w-full text-2xl font-serif text-rg-red">
+                    Leaderboard
+                </CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col font-mono text-sm p-0 rounded-lg text-white">
+                <div className="min-h-[60px] grid grid-cols-[60px_1fr_1fr_1fr_1fr] gap-4 border-b border-white px-6 items-center">
+                    <p>Rank</p>
+                    <p>Wallet Address</p>
+                    <p>Token Holdings</p>
+                    <p>Staked Amount</p>
+                    <p>Voting Participation</p>
+                </div>
+
+                {tokenHolders
+                    .slice()
+                    .sort((a, b) => b.holdings - a.holdings)
+                    .map((holder, i) => (
+                        <div
+                            key={holder.address}
+                            className="min-h-[60px] grid grid-cols-[60px_1fr_1fr_1fr_1fr] gap-4 border-b border-white items-center px-6"
+                        >
+                            <p>{getRankFromIndex(i)}</p>
+                            <p>{holder.address}</p>
+                            <p>
+                                {holder.holdings.toLocaleString()}{" "}
+                                {selectedToken}
+                            </p>
+                            <p>
+                                {holder.stakedAmount.toLocaleString()}{" "}
+                                {selectedToken}
+                            </p>
+                            <p>{holder.votingParticipation}</p>
+                        </div>
+                    ))}
+            </CardContent>
+        </Card>
+    );
+};
 
 export { Leaderboard };
+
+function getRankFromIndex(i) {
+    return new Intl.PluralRules("en", {
+        type: "ordinal",
+    }).select(i + 1) === "one"
+        ? `${i + 1}st`
+        : new Intl.PluralRules("en", {
+              type: "ordinal",
+          }).select(i + 1) === "two"
+        ? `${i + 1}nd`
+        : new Intl.PluralRules("en", {
+              type: "ordinal",
+          }).select(i + 1) === "few"
+        ? `${i + 1}rd`
+        : (i + 1) % 10 === 1 && (i + 1) % 100 !== 11
+        ? `${i + 1}st`
+        : (i + 1) % 10 === 2 && (i + 1) % 100 !== 12
+        ? `${i + 1}nd`
+        : (i + 1) % 10 === 3 && (i + 1) % 100 !== 13
+        ? `${i + 1}rd`
+        : `${i + 1}th`;
+}
